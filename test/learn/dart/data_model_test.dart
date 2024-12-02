c2b5a162e5657f0b55f6a09afcb95fd2e8ab927e
cheng.jared <chengjundong@gmail.com>
2024-12-03 06:33:11 +0800

    add data_model UT

--- test/learn/dart/data_model_test.dart
+++ test/learn/dart/data_model_test.dart
@@ -32,4 +32,43 @@ void main() {
     expect(data.method, 'GET');
     expect(data.requestPrams, '11');
   });
+
+  test('DataModel construction success', () {
+    // Act
+    DataModel dataModel = DataModel(
+      code: 0,
+      data: Data(
+        code: 0,
+        method: 'GET',
+        requestPrams: '11',
+      ),
+      msg: 'success',
+    );
+
+    // Assert
+    expect(dataModel.code, 0);
+    expect(dataModel.data!.code, 0);
+    expect(dataModel.data!.method, 'GET');
+    expect(dataModel.data!.requestPrams, '11');
+    expect(dataModel.msg, 'success');
+  });
+
+  test("DataModel fromJson success", () {
+    // Arrange
+    final Map<String, dynamic> json = {
+      "code": 11,
+      "msg": "success",
+      "data": {"code": 12, "method": "GET", "requestPrams": "11"}
+    };
+
+    // Act
+    final dataModel = DataModel.fromJson(json);
+
+    // Assert
+    expect(dataModel.code, 11);
+    expect(dataModel.data!.code, 12);
+    expect(dataModel.data!.method, 'GET');
+    expect(dataModel.data!.requestPrams, '11');
+    expect(dataModel.msg, 'success');
+  });
 }
